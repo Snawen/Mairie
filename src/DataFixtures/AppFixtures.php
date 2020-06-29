74287c55ac9df10b8a02a3e3b13e9377d22b914d
Snawen <axel.isaia84@gmail.com>
2020-06-29 16:57:47 +0200

    Ouais

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Articles;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        // $product = new Product();
+        // $manager->persist($product);
+        $art = new Articles();
+        $art->setTitre($faker->word);
+        $art->setAuteur($faker->name);
+        $art->setContenu($faker->sentence($nbWords = 6, $variableNbWords = true));
+        $art->setDate($faker->dateTime);
+        
+        $manager->persist($art);
+        
+        
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticlesRepository;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticlesRepository::class)
+ */
+class Articles
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Contenu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Auteur;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): self
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): self
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+
+    public function getDate(): ?DateTime
+    {
+        return $this->Date;
+    }
+
+    public function setDate(DateTime $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+}
